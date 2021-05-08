344c57ce80f33ca6bac89b39c97f895ead1d6417
likith1999 <likith.vagicharla1999@gmail.com>
2021-05-08 19:27:30 +0530

    Adding the project

---
+++ src/test/java/helloworld_dev/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld_dev;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld(){
+		System.out.println("Hello World");
+	}
+
+}
